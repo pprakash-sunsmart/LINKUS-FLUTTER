a200fb00ab868a0ef272988914b6ddbf47ad67f1
faz4563 <79906416+faz4563@users.noreply.github.com>
2022-06-29 11:56:49 +0530

    secondary commit

---
+++ lib/screens/LandingScreen.dart
@@ -0,0 +1,19 @@
+// ignore_for_file: camel_case_types, prefer_const_constructors, avoid_unnecessary_containers, file_names
+
+import 'package:flutter/cupertino.dart';
+
+class landingPage extends StatefulWidget {
+  const landingPage({super.key});
+
+  @override
+  State<landingPage> createState() => _landingPageState();
+}
+
+class _landingPageState extends State<landingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('data'),
+    );
+  }
+}
