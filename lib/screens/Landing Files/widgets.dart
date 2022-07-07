61cde800f829ebbd5e312dd55775c98b85029951
Abishlin <108339660+Abishlin@users.noreply.github.com>
2022-07-07 09:59:46 +0530

    Smiley work incomplete

--- lib/screens/Landing Files/widgets.dart
+++ lib/screens/Landing Files/widgets.dart
@@ -1,5 +1,8 @@
-// ignore_for_file: prefer_const_constructors, prefer_typing_uninitialized_variables, non_constant_identifier_names, prefer_const_literals_to_create_immutables, avoid_print, camel_case_types, must_be_immutable, unrelated_type_equality_checks
+// ignore_for_file: prefer_const_constructors, prefer_typing_uninitialized_variables, non_constant_identifier_names, prefer_const_literals_to_create_immutables, avoid_print, camel_case_types
 
+import 'dart:io';
+
+import 'package:emoji_picker_flutter/emoji_picker_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:linkus/screens/chatscreen%20Files/dataList.dart';
 
@@ -25,8 +28,6 @@ class ChatList extends StatefulWidget {
   @override
   State<ChatList> createState() => _ChatListState();
 }
-
-final TextEditingController chatController = TextEditingController();
 
 class _ChatListState extends State<ChatList> {
   @override
@@ -77,104 +78,140 @@ class ChatInputBox extends StatefulWidget {
 
 class _ChatInputBoxState extends State<ChatInputBox> {
   GlobalKey<FormState> globalFormKey = new GlobalKey<FormState>();
-  bool MicIcon = true;
+  bool SndIcon = false;
 
-  // var text = chatController.value.text.length;
-  onTap(text) {
-    setState(() {
-      if (chatController.text.trim() == "") {
-        MicIcon = true;
-      } else {
-        MicIcon = false;
-      }
-    });
+  bool emojiShowing = false;
+  final TextEditingController _controller = TextEditingController();
+
+  _onEmojiSelected(Emoji emoji) {
+    _controller
+      ..text += emoji.emoji
+      ..selection = TextSelection.fromPosition(
+          TextPosition(offset: _controller.text.length));
   }
 
-  @override
-  void initState() {
-    onTap(MicIcon);
-    super.initState();
+  _onBackspacePressed() {
+    _controller
+      ..text = _controller.text.characters.skipLast(1).toString()
+      ..selection = TextSelection.fromPosition(
+          TextPosition(offset: _controller.text.length));
   }
 
   @override
   Widget build(BuildContext context) {
     return Form(
-      key: globalFormKey,
-      child: Container(
-        height: 60,
-        color: Color.fromRGBO(1, 123, 255, 1),
-        child: Padding(
-          padding: const EdgeInsets.all(9),
-          child: Row(
-            children: [
-              Flexible(
-                flex: 1,
-                child: Container(
+        key: globalFormKey,
+        child: Container(
+            height: 60,
+            color: Color.fromRGBO(1, 123, 255, 1),
+            child: Padding(
+                padding: const EdgeInsets.all(9),
+                child: Row(children: [
+                  Flexible(
+                      flex: 1,
+                      child: Column(
+                        children: [
+                          Container(
+                              decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(20)),
+                              child: TextFormField(
+                                  cursorHeight: 16,
+                                  controller: widget.controller,
+                                  cursorColor: Colors.grey.shade900,
+                                  decoration: InputDecoration(
+                                      enabledBorder: InputBorder.none,
+                                      contentPadding:
+                                          EdgeInsets.symmetric(vertical: 8),
+                                      hintText: 'Type a message',
+                                      prefixIcon: IconButton(
+                                          onPressed: () {
+                                            setState(() {
+                                              emojiShowing = !emojiShowing;
+                                            });
+                                          },
+                                          icon: Image(
+                                              image: AssetImage(
+                                                  'assets/images/smiley.png'))),
+                                      suffixIcon: IconButton(
+                                        onPressed: () {
+                                          print('dsa');
+                                        },
+                                        icon: InkWell(
+                                          onTap: () {},
+                                          child: Image(
+                                            image: AssetImage(
+                                                'assets/images/attachments.png'),
+                                            height: 50,
+                                          ),
+                                        ),
+                                      )))),
+                          Offstage(
+                            offstage: !emojiShowing,
+                            //changed
+                            child: SizedBox(
+                              height: 250,
+                              child: EmojiPicker(
+                                  onEmojiSelected:
+                                      (Category category, Emoji emoji) {
+                                    _onEmojiSelected(emoji);
+                                  },
+                                  onBackspacePressed: _onBackspacePressed,
+                                  config: Config(
+                                      columns: 7,
+                                      emojiSizeMax:
+                                          32 * (Platform.isIOS ? 1.30 : 1.0),
+                                      verticalSpacing: 0,
+                                      horizontalSpacing: 0,
+                                      gridPadding: EdgeInsets.zero,
+                                      initCategory: Category.RECENT,
+                                      bgColor: const Color(0xFFF2F2F2),
+                                      indicatorColor: Colors.blue,
+                                      iconColor: Colors.grey,
+                                      iconColorSelected: Colors.blue,
+                                      progressIndicatorColor: Colors.blue,
+                                      backspaceColor: Colors.blue,
+                                      skinToneDialogBgColor: Colors.white,
+                                      skinToneIndicatorColor: Colors.grey,
+                                      enableSkinTones: true,
+                                      showRecentsTab: true,
+                                      recentsLimit: 28,
+                                      replaceEmojiOnLimitExceed: false,
+                                      noRecents: const Text(
+                                        'No Recents',
+                                        style: TextStyle(
+                                            fontSize: 20,
+                                            color: Colors.black26),
+                                        textAlign: TextAlign.center,
+                                      ),
+                                      tabIndicatorAnimDuration:
+                                          kTabScrollDuration,
+                                      categoryIcons: const CategoryIcons(),
+                                      buttonMode: ButtonMode.MATERIAL)),
+                            ),
+                          ),
+                        ],
+                      )), // iconSize: 25,
+
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Container(
+                    width: 40,
+                    height: 40,
                     decoration: BoxDecoration(
                         color: Colors.white,
                         borderRadius: BorderRadius.circular(20)),
-                    child: TextFormField(
-                      cursorHeight: 16,
-                      controller: chatController,
-                      onTap: onTap(MicIcon),
-                      onChanged: onTap,
-                      // autovalidateMode: AutovalidateMode.always,
-                      cursorColor: Colors.grey.shade900,
-                      // onTap: onTap(),
-                      // onTap: onTap(),
-                      decoration: InputDecoration(
-                          enabledBorder: InputBorder.none,
-                          contentPadding: EdgeInsets.symmetric(vertical: 8),
-                          hintText: 'Type a message',
-                          prefixIcon: IconButton(
-                              onPressed: () {},
-                              icon: Image(
-                                  image:
-                                      AssetImage('assets/images/smiley.png'))),
-                          suffixIcon: IconButton(
-                            onPressed: () {
-                              print('dsa');
-                            },
-                            icon: Image(
-                              image:
-                                  AssetImage('assets/images/attachments.png'),
-                              height: 50,
-                            ),
-                            // iconSize: 25,
-                          )),
-                    )),
-              ),
-              SizedBox(
-                width: 10,
-              ),
-              Container(
-                  width: 40,
-                  height: 40,
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(20)),
-                  child: MicIcon
-                      ? InkWell(
-                          onTap: () {},
-                          child: Image(
-                            image: AssetImage('assets/images/mic.png'),
-                          ))
-                      : Padding(
-                          padding: const EdgeInsets.only(left: 3),
-                          child: Icon(
-                            Icons.send,
-                            color: Color.fromRGBO(1, 123, 255, 1),
-                            size: 20,
-                          ),
-                        )),
-              SizedBox(
-                width: 5,
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
+                    child: GestureDetector(
+                      onLongPress: () {},
+                      child: SndIcon
+                          ? Image(
+                              image: AssetImage('assets/images/mic.png'),
+                            )
+                          : Icon(Icons.send),
+                    ),
+                  )
+                ]))));
   }
 }
 
